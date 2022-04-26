2dd7e5695758f65b683642dbfc1588e2d856dc95
Madeline Scyphers <madelinescyphers@gmail.com>
2022-04-25 20:46:32 -0400

    bugs: fix synthetic funcs

--- optiwrap/synthetic_funcs/funcs.py
+++ optiwrap/synthetic_funcs/funcs.py
@@ -1,9 +1,6 @@
 import numpy as np
-from ax.utils.common.docutils import copy_doc
 from ax.utils.measurement.synthetic_functions import from_botorch, SyntheticFunction
 from botorch.test_functions.synthetic import Ackley
-
-ackley = from_botorch(Ackley(dim=4))
 
 
 def map_range(value, old_min, old_max, new_min, new_max):
@@ -12,9 +9,11 @@ def map_range(value, old_min, old_max, new_min, new_max):
     return (((value - old_min) * NewRange) / OldRange) + new_min
 
 
-class FakePalm(ackley):
-    @copy_doc(SyntheticFunction._f)
-    def _f(self, X: np.ndarray) -> float:
+class FakePalm(Ackley):
+    def __init__(self, *args, **kwargs):
+        dim = 4
+        super().__init__(dim=dim, *args, **kwargs)
+    def evaluate_true(self, X: np.ndarray) -> float:
 
         X[0] = map_range(X[0], 350, 950, -32.768, 32.768)
         X[1] = map_range(X[1], 0.15, 0.9, -32.768, 32.768)
@@ -29,3 +28,5 @@ class FakePalm(ackley):
         #     "house_ratio": .15 - .9
         #     "ground_ratio": .25 - .8
         #     "mean_lai": 2.0 - 6.0
+
+fake_palm = from_botorch(FakePalm())
