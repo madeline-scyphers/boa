872132961fb67ead7e0f269a08417a278383aa2c
Madeline Scyphers <madelinescyphers@gmail.com>
2022-04-25 21:12:27 -0400

    fakepalm uses tensors now

--- optiwrap/synthetic_funcs/funcs.py
+++ optiwrap/synthetic_funcs/funcs.py
@@ -1,6 +1,7 @@
 import numpy as np
 from ax.utils.measurement.synthetic_functions import from_botorch, SyntheticFunction
 from botorch.test_functions.synthetic import Ackley
+from torch import Tensor
 
 
 def map_range(value, old_min, old_max, new_min, new_max):
@@ -13,20 +14,12 @@ class FakePalm(Ackley):
     def __init__(self, *args, **kwargs):
         dim = 4
         super().__init__(dim=dim, *args, **kwargs)
-    def evaluate_true(self, X: np.ndarray) -> float:
+    def evaluate_true(self, X: Tensor) -> float:
+        X[..., 0] = map_range(X[..., 0], 350, 950, -32.768, 32.768)
+        X[..., 1] = map_range(X[..., 1], 0.15, 0.9, -32.768, 32.768)
+        X[..., 2] = map_range(X[..., 2], 0.25, 0.8, -32.768, 32.768)
+        X[..., 3] = map_range(X[..., 3], 2.0, 6.0, -32.768, 32.768)
 
-        X[0] = map_range(X[0], 350, 950, -32.768, 32.768)
-        X[1] = map_range(X[1], 0.15, 0.9, -32.768, 32.768)
-        X[2] = map_range(X[2], 0.25, 0.8, -32.768, 32.768)
-        X[3] = map_range(X[3], 2.0, 6.0, -32.768, 32.768)
-
-        return super()._f(X)
-
-        # -32.768, 32.768
-
-        #     "plot_footprint": 350 - 950
-        #     "house_ratio": .15 - .9
-        #     "ground_ratio": .25 - .8
-        #     "mean_lai": 2.0 - 6.0
+        return super().evaluate_true(X)
 
 fake_palm = from_botorch(FakePalm())
