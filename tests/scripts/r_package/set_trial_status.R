eca6c532c296fbc1abc981320e12934199d5ea03
Madeline Scyphers <madelinescyphers@gmail.com>
2022-10-06 13:45:33 -0400

    Change set trial status to only write on complete

--- tests/scripts/r_package/set_trial_status.R
+++ tests/scripts/r_package/set_trial_status.R
@@ -10,12 +10,9 @@ is_completed <- file.exists(file.path(trial_dir, "run_model_from_wrapper.json"))
 
 if (is_completed) {
     trial_status <- "COMPLETED"
-} else {
-    trial_status <- "RUNNING"
+    out_data <- list(
+        TrialStatus=unbox(trial_status)
+    )
+    json_data <- toJSON(out_data, pretty = TRUE)
+    write(json_data, file.path(trial_dir, "set_trial_status_from_wrapper.json"))
 }
-
-out_data <- list(
-    TrialStatus=unbox(trial_status)
-)
-json_data <- toJSON(out_data, pretty = TRUE)
-write(json_data, file.path(trial_dir, "set_trial_status_from_wrapper.json"))
