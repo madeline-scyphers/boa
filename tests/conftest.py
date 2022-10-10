d28181ab92fbe9767b7d1ffaaaef37d524d81bea
Justine Missik <missik.2@osu.edu>
2022-10-10 13:05:08 -0400

    Update paths in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,48 +5,48 @@ import pytest
 import boa.__main__ as dunder_main
 import boa.test_scripts.run_branin as run_branin
 from boa import cd_and_cd_back, load_yaml
+from boa.definitions import ROOT
 
-ROOT = Path(__file__).parent.parent
 TEST_DIR = ROOT / "tests"
 TEST_CONFIG_DIR = TEST_DIR / "test_configs"
 
 
 @pytest.fixture
 def synth_config():
-    config_path = Path(__file__).parent / "test_configs/test_config_synth.yaml"
+    config_path = TEST_CONFIG_DIR / "test_config_synth.yaml"
     return load_yaml(config_path)
 
 
 @pytest.fixture
 def metric_config():
-    config_path = Path(__file__).parent / "test_configs/test_config_metric.yaml"
+    config_path = TEST_CONFIG_DIR / "test_config_metric.yaml"
     return load_yaml(config_path)
 
 
 @pytest.fixture
 def gen_strat1_config():
-    config_path = Path(__file__).parent / "test_configs/test_config_gen_strat1.yaml"
+    config_path = TEST_CONFIG_DIR / "test_config_gen_strat1.yaml"
     return load_yaml(config_path)
 
 
 @pytest.fixture
 def soo_config():
     """ScalarizedObjective Optimization config"""
-    config_path = Path(__file__).parent / "test_configs/test_config_soo.yaml"
+    config_path = TEST_CONFIG_DIR / "test_config_soo.yaml"
     return load_yaml(config_path)
 
 
 @pytest.fixture
 def moo_config():
     """MultiObjective Optimization config"""
-    config_path = Path(__file__).parent / "test_configs/test_config_moo.yaml"
+    config_path = TEST_CONFIG_DIR / "test_config_moo.yaml"
     return load_yaml(config_path)
 
 
 @pytest.fixture
 def denormed_param_parse_config():
     """MultiObjective Optimization config"""
-    config_path = Path(__file__).parent / "test_configs/test_config_param_parse.yaml"
+    config_path = TEST_CONFIG_DIR / "test_config_param_parse.yaml"
     return load_yaml(config_path)
 
 
@@ -62,13 +62,13 @@ def metric_optimization_options(metric_config):
 
 @pytest.fixture
 def cd_to_root_and_back():
-    with cd_and_cd_back(Path(__file__).resolve().parent.parent):
+    with cd_and_cd_back(ROOT):
         yield
 
 
 @pytest.fixture(scope="session")
 def cd_to_root_and_back_session():
-    with cd_and_cd_back(Path(__file__).resolve().parent.parent):
+    with cd_and_cd_back(ROOT):
         yield
 
 
