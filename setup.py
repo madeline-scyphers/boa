4b8d07e3689c9bb182b92779a60351857400222b
Madeline Scyphers <madelinescyphers@gmail.com>
2022-12-02 11:02:30 -0500

    Move to using pyproject.toml instead of setup.py
    most of project metadata is now describe in pyproject.toml now
    since that is where python is going to.
    The dependencies are pulled from the environment.yml files dynamically and listed in the setup.py file.
    Everything else is listed in the pyproject.toml file.
    Also added a test in the CI script for github to test if the project builds.

--- setup.py
+++ setup.py
@@ -1,81 +1,74 @@
-#!/usr/bin/env python3
+from pathlib import Path
 
-import io
-import os
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-
-# Get version
-def read(*names, **kwargs):
-    with io.open(os.path.join(os.path.dirname(__file__), *names), encoding=kwargs.get("encoding", "utf8")) as fp:
-        return fp.read()
+BASE_ENV = Path(__file__).resolve().parent / "environment.yml"
+DEV_ENV = Path(__file__).resolve().parent / "environment_dev_update.yml"
 
 
-readme = open("README.md").read()
+# dictionary of any package renaming from conda package names to PyPI names
+# ex) PyTorch is pytorch on conda, but listed as torch on PyPI
+# also if you are installing a pkg from a git rep, you should remap it
+# to "git+https/etc": "pkg_name @ git+https/etc"
+pkg_renames = {"pytorch": "torch"}
 
-# set the install requirements
-install_requires = [
-    "torch>=1.9",
-    "torchvision",
-    "torchaudio",
-    "gpytorch>=1.5",
-    "botorch>=0.5",
-    "ax-platform>=0.2.8",
-    "scikit-learn",
-    "scipy",
-    "pandas",
-    "numpy",
-    "click",
-    "sqlalchemy",
-    "PyYAML",
-]
 
-# Run the setup
+# list of any packages in your environment files you don't want to include
+# They have to be the exact text of the pkg name, including any version info
+# (example, numpy<=42)
+# You should also not include any packages that aren't python packages
+# such as nodejs, git, GDAL, etc.
+pkgs_to_exclude = []
+
+
+def remove_non_pkgs(pkg: str):
+    is_pip = "-pip" in pkg
+    is_python = "-python=" in pkg
+    is_self = "--e." in pkg
+    is_inline_cmt = pkg.startswith("#")
+    if not is_pip and not is_python and not is_self and not is_inline_cmt:
+        return True
+
+
+def exclude_excluded_pkgs(pkg: str):
+    if pkg not in pkgs_to_exclude:
+        return True
+
+
+def strip_non_pkg_chars(pkg):
+    return (
+        pkg.lstrip("-")  # remove leading dashes from listing packages
+        .split("::")[-1]  # on pkgs that specify directly their channel, take pkg, not channel
+        .split("#")[0]  # split inline comments and take pkg
+        .strip()
+    )
+
+
+def rename_pkgs(pkg):
+    if pkg in pkg_renames:
+        return pkg_renames[pkg]
+    return pkg
+
+
+def process_file(path):
+    with open(path, "r") as f:
+        file = f.read()
+    deps = file.split("dependencies:")[-1].strip().replace(" ", "").splitlines()
+    deps = [dep for dep in deps if dep.startswith("-")]
+    almost_pkgs = filter(remove_non_pkgs, deps)
+    pkgs = map(strip_non_pkg_chars, almost_pkgs)
+    pkgs = filter(exclude_excluded_pkgs, pkgs)
+    pkgs = map(rename_pkgs, pkgs)
+    return list(pkgs)
+
+
+pkgs_to_exclude = list(map(strip_non_pkg_chars, pkgs_to_exclude))
+
+base_pkgs = process_file(BASE_ENV)
+
+dev_pkgs = [pkg for pkg in process_file(DEV_ENV) if pkg not in base_pkgs]
+
 setup(
-    name="boa",
-    use_scm_version={"write_to": "boa/_version.py"},
-    setup_requires=["setuptools_scm"],
-    include_package_data=True,
-    description="An implementation of Gaussian Processes in Pytorch",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    author="Madeline Scyphers, Justine Missik",
-    url="https://github.com/madeline-scyphers/boa",
-    author_email="madelinescyphers@gmail.com, jemissik@gmail.com",
-    project_urls={
-        "Source": "https://github.com/madeline-scyphers/boa",
-    },
-    license="MIT",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Operating System :: OS Independent",
-    ],
-    packages=find_packages(exclude=["test", "test.*"]),
-    python_requires=">=3.7",
-    install_requires=install_requires,
-    extras_require={
-        "dev": [
-            "black<=22.3.0",
-            "black[jupyter]",
-            "isort",
-            "flake8",
-            "flakeheaven>=3.0.0",
-            "pytest",
-            "invoke",
-            "setuptools_scm",
-        ],
-        "docs": [
-            "sphinx",
-            "myst-nb",
-            "pydata-sphinx-theme",
-            "sphinxext-remoteliteralinclude",
-        ],
-        "examples": ["jupyter", "hvplot"],
-    },
+    install_requires=base_pkgs,
+    extras_require={"dev": dev_pkgs},
 )
