2651b4ae33f6043a312259f8ea8666c5b5da0aa8
Madeline Scyphers <madelinescyphers@gmail.com>
2023-02-16 23:19:00 -0500

    Condense streamline R and streamline fail R to one folder with comments for fail config on why it fails

--- tests/integration_tests/test_dunder_main.py
+++ tests/integration_tests/test_dunder_main.py
@@ -36,9 +36,10 @@ def test_calling_command_line_test_script_doesnt_error_out_and_produces_correct_
 
 
 # parametrize the test to use the full version (all scripts) or the light version (only run_model.R)
+# or parametrize the test to use the streamlined version (doesn't use trial_status.json, only use output.json)
 @pytest.mark.parametrize(
     "r_scripts_run",
-    ["full", "light"],
+    ["full", "light", "streamlined"],
     indirect=True,
 )
 @pytest.mark.skipif(not R_INSTALLED, reason="requires R to be installed")
@@ -50,23 +51,8 @@ def test_calling_command_line_r_test_scripts(r_scripts_run):
     assert len(scheduler.experiment.trials) == total_trials
 
 
-# parametrize the test to use the streamlined version (doesn't use trial_status.json, only use output.json)
-@pytest.mark.parametrize(
-    "r_scripts_run",
-    ["streamlined"],
-    indirect=True,
-)
-@pytest.mark.skipif(not R_INSTALLED, reason="requires R to be installed")
-def test_calling_streamlined_cli_interface(r_scripts_run):
-    scheduler = r_scripts_run
-    wrapper = scheduler.experiment.runner.wrapper
-    config = wrapper.config
-    total_trials = config["optimization_options"]["scheduler"]["total_trials"]
-    assert len(scheduler.experiment.trials) == total_trials
-
-
 @pytest.mark.skipif(not R_INSTALLED, reason="requires R to be installed")
 def test_cli_interface_with_failing_test_that_sends_back_failed_trial_status():
     with pytest.raises(FailureRateExceededError):
-        config_path = ROOT / "tests" / f"scripts/other_langs/r_package_streamlined_fail/config.yaml"
+        config_path = ROOT / "tests" / f"scripts/other_langs/r_package_streamlined/config_fail.yaml"
         dunder_main.main(split_shell_command(f"--config-path {config_path} -td"), standalone_mode=False)
